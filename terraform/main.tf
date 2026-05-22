d55a89ae4802de2c5507a926153037ffe1e535f3
Anushri Salke <salkeanushri@gmail.com>
2026-05-22 22:27:39 +0530

    Terraform AWS infrastructure setup

---
+++ terraform/main.tf
@@ -0,0 +1,35 @@
+resource "aws_security_group" "devsecops_sg" {
+  name = "devsecops-security-group"
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "devsecops_server" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+
+  security_groups = [aws_security_group.devsecops_sg.name]
+
+  tags = {
+    Name = "AI-DevSecOps-Server"
+  }
+}
